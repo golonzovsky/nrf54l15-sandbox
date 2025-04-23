2c5537c0d5234e016ed5c93283bd12273774e5d3
golonzovsky <alex.golonzovsky@gmail.com>
2025-04-23 16:32:53 +0200

    blinky works

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 use core::{panic::PanicInfo, ptr::write_volatile};
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
-use rtt_target::{rprintln, rtt_init_print};
 
 #[panic_handler]
 fn panic(_i: &PanicInfo) -> ! {
@@ -13,11 +12,8 @@ fn panic(_i: &PanicInfo) -> ! {
 
 #[entry]
 fn main() -> ! {
-    //rtt_init_print!();
-    //rprintln!("hello world");
-
     // set p1.14 as out
-    const GPIO_PIN1CNF14_ADDR: *mut u32 = 0x400D_82B8 as *mut u32;
+    const GPIO_PIN1CNF14_ADDR: *mut u32 = 0x500D_82B8 as *mut u32;
     const DIR_OUTPUT_POS: u32 = 0;
     const PINCNF_DRIVE_LED: u32 = 1 << DIR_OUTPUT_POS;
     unsafe {
@@ -25,18 +21,16 @@ fn main() -> ! {
     }
 
     // write to p1.14
-    const GPIO_PINCNF_P1_BASE_ADDR: *mut u32 = 0x400D_8200 as *mut u32;
-    const GPIO_OUT_ADDR: *mut u32 = GPIO_PINCNF_P1_BASE_ADDR;
+    const GPIO1_OUT_ADDR: *mut u32 = 0x500D_8200 as *mut u32;
     const GPIO1_OUT_LED3_POS: u32 = 14;
-    let mut is_on = false;
+    let mut is_on = true;
     loop {
-        //rprintln!("hello world");
         unsafe {
-            write_volatile(GPIO_PIN1CNF14_ADDR, (is_on as u32) << GPIO1_OUT_LED3_POS);
+            write_volatile(GPIO1_OUT_ADDR, (is_on as u32) << GPIO1_OUT_LED3_POS);
         }
-        for _ in 0..100_000 {
+        for _ in 0..500_000 {
             nop();
         }
-        is_on = !is_on
+        is_on = !is_on;
     }
 }
