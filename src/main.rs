254879fee57d68654d753a5c122f7bf87da3d9fb
golonzovsky <alex.golonzovsky@gmail.com>
2025-04-23 17:42:32 +0200

    works with PAC

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use core::{panic::PanicInfo, ptr::write_volatile};
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
+use nrf_pac as pac;
 
 #[panic_handler]
 fn panic(_i: &PanicInfo) -> ! {
@@ -12,23 +13,14 @@ fn panic(_i: &PanicInfo) -> ! {
 
 #[entry]
 fn main() -> ! {
-    // set p1.14 as out
-    const GPIO_PIN1CNF14_ADDR: *mut u32 = 0x500D_82B8 as *mut u32;
-    const DIR_OUTPUT_POS: u32 = 0;
-    const PINCNF_DRIVE_LED: u32 = 1 << DIR_OUTPUT_POS;
-    unsafe {
-        write_volatile(GPIO_PIN1CNF14_ADDR, PINCNF_DRIVE_LED);
-    }
+    pac::P1_S
+        .pin_cnf(14)
+        .write(|w| w.set_dir(pac::gpio::vals::Dir::OUTPUT));
 
-    // write to p1.14
-    const GPIO1_OUT_ADDR: *mut u32 = 0x500D_8200 as *mut u32;
-    const GPIO1_OUT_LED3_POS: u32 = 14;
     let mut is_on = true;
     loop {
-        unsafe {
-            write_volatile(GPIO1_OUT_ADDR, (is_on as u32) << GPIO1_OUT_LED3_POS);
-        }
-        for _ in 0..500_000 {
+        pac::P1_S.out().write(|w| w.set_pin(14, is_on));
+        for _ in 0..100_000 {
             nop();
         }
         is_on = !is_on;
